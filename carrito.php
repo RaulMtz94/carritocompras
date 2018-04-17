12d929c5fa461e701d068669c751336bbfd2b5f0
joselinevaldez <joselinevg4@gmail.com>
2018-04-17 11:18:54 -0600

    ultimocarrito

--- carrito.php
+++ carrito.php
@@ -10,6 +10,22 @@ if(isset($_SESSION['user'])) {
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
 </head>
+<script>
+    var  arreglo_libros = [];
+    var x = 0;
+    function agregarcarrito(libro){
+       arreglo_libros.push(libro);
+       alert("producto agregado..")
+      
+       
+
+    }
+    function vercarrito(){
+        for (i=0;i<arreglo_libros.length;i++) {
+        alert("imprimir",arreglo_libros[i]);
+     }
+    }
+</script>
 <style>
     body{
         background-image: url("imagenes/fondo.jpg");
@@ -80,7 +96,7 @@ if(isset($_SESSION['user'])) {
         <div class="menu">
             <label class="bienvenido"><?php echo "Bienvenido : ",$_SESSION['user']?></label>
             
-            <a class="carrito" href="#"><img src="imagenes/carrito.png" width="40" height="40"  title="ir al carrito de compras"></a>
+            <a class="carrito" href="#"onclick="vercarrito()"><img src="imagenes/carrito.png" width="40" height="40"  title="ir al carrito de compras"></a>
             <a href="logout.php"><img src="imagenes/logout.png" width="40" height="40"  title="cerrar sesion"></a>
    
         </div>
@@ -91,14 +107,14 @@ if(isset($_SESSION['user'])) {
                 <h4 align="center">precio : $399.99</h4>
                 <img class="imagenes_libros" src="imagenes/java.jpg" width="100" height="150"  >
                <br>
-                <a class="agregar_carrito" href="#"><img src="imagenes/carrito.png" width="30" height="30">Agregar al carrito</a>
+                <a class="agregar_carrito" href="#"onclick="agregarcarrito('1');"><img src="imagenes/carrito.png" width="30" height="30">Agregar al carrito</a>
                 </div>
                 <div class="libros_abajo">
                        <h3 align="center">Node js</h3>
                         <h4 align="center">precio : $289.99</h4>
                         <img class="imagenes_libros" src="imagenes/nodejs.jpg" width="100" height="150"  >
                         <br>
-                        <a class="agregar_carrito" href="#"><img src="imagenes/carrito.png" width="30" height="30">Agregar al carrito</a>
+                        <a class="agregar_carrito" href="#" onclick="agregarcarrito('2');"><img src="imagenes/carrito.png" width="30" height="30">Agregar al carrito</a>
                 </div>
                 
                
@@ -108,14 +124,14 @@ if(isset($_SESSION['user'])) {
                 <h3 align="center">PHP 7</h3>
                 <h4 align="center">precio : $528.67</h4>
                 <img class="imagenes_libros" src="imagenes/php.jpg" width="100" height="150" ><br>
-                <a class="agregar_carrito" href="#"><img src="imagenes/carrito.png" width="30" height="30">Agregar al carrito</a>
+                <a class="agregar_carrito" href="#"onclick="agregarcarrito('3');"><img src="imagenes/carrito.png" width="30" height="30">Agregar al carrito</a>
                 </div>
                 <div class="libros_abajo">
                        
                         <h3 align="center">HTML 5</h3>
                         <h4 align="center">precio : $419.99</h4>
                         <img class="imagenes_libros" src="imagenes/html.jpg" width="100" height="150" ><br>
-                        <a class="agregar_carrito" href="#"><img src="imagenes/carrito.png" width="30" height="30">Agregar al carrito</a>
+                        <a class="agregar_carrito" href="#"onclick="agregarcarrito('4');"><img src="imagenes/carrito.png" width="30" height="30">Agregar al carrito</a>
            
                     </div>
                
@@ -125,14 +141,14 @@ if(isset($_SESSION['user'])) {
                 <h3 align="center">Ubuntu</h3>
                 <h4 align="center">precio : $749.99</h4>
                 <img class="imagenes_libros" src="imagenes/ubuntu.jpg" width="100" height="150" ><br>
-                <a class="agregar_carrito" href="#"><img src="imagenes/carrito.png" width="30" height="30">Agregar al carrito</a>
+                <a class="agregar_carrito" href="#"onclick="agregarcarrito('5');"><img src="imagenes/carrito.png" width="30" height="30">Agregar al carrito</a>
            
                 <div class="libros_abajo">
                        
                         <h3 align="center">Android</h3>
                         <h4 align="center">precio : $1099.99</h4>
                         <img class="imagenes_libros" src="imagenes/android.jpg" width="100" height="150" ><br>
-                        <a class="agregar_carrito" href="#"><img src="imagenes/carrito.png" width="30" height="30">Agregar al carrito</a>
+                        <a class="agregar_carrito" href="#"onclick="agregarcarrito('6');"><img src="imagenes/carrito.png" width="30" height="30">Agregar al carrito</a>
            
                     </div>
                
